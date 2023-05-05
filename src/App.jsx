e7938b760ce9b9571d0707f195833577482544f7
Hanad Mohamed <hanaddaahir124@gmail.com>
2023-05-05 16:41:17 +0300

    update the login form

--- src/App.jsx
+++ src/App.jsx
@@ -110,6 +110,8 @@ const App = () => {
                             <RequireAuth>
                               <Supplier />
                             </RequireAuth>
+
+                            
                           }
                         />
                         <Route
